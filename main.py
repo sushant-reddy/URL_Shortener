40e7b40265cdd57c6891b045fb990d283b21c41e
Sushant Reddy <63240393+sushant-reddy@users.noreply.github.com>
2023-05-19 09:48:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from waitress import serve
 import random
 import string
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://sushant:12345@cluster0.mkt9avr.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["URL_DB"]
+collection = db["URL"]
 
 app = Flask(__name__)
 
@@ -16,12 +22,24 @@ def post_data():
     url = data.get('payload')
     auth_token = request.headers.get('Auth-Token')
     print(url)
-    
+
     short_string = generate_short_string()
-    short_url = f"https://test.ly/{short_string}"
+    short_url = f"http://localhost:8080/{short_string}"
     print(short_url)
-    
+
+    urldata = {"URL": url, "short_url": short_url}
+    collection.insert_one(urldata)
     return short_url
+
+
+@app.route('/<short_string>', methods=['GET'])
+def redirect_to_url(short_string):
+    urldata = collection.find_one({"short_url": f"http://localhost:8080/{short_string}"})
+    if urldata:
+        finalurl = urldata.get('URL')
+        return redirect(finalurl)
+    else:
+        return "Invalid short URL."
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
